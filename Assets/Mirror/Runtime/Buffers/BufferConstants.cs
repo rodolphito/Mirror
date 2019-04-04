be8c61b4672aba52ae2da6c31f1542d5fef066b7
Rodol Phito <rodolvertice@gmail.com>
2019-04-04 06:56:18 -0700

    Remove unused size constants

--- Assets/Mirror/Runtime/Buffers/BufferConstants.cs
+++ Assets/Mirror/Runtime/Buffers/BufferConstants.cs
@@ -6,13 +6,6 @@ namespace Mirror.Buffers
 {
     public static class BufferConstants
     {
-        public const int SizeOfByte = 1;
-        public const int SizeOfShort = 2;
-        public const int SizeOfInt = 4;
-        public const int SizeOfFloat = 4;
-        public const int SizeOfLong = 8;
-        public const int SizeOfDouble = 8;
-
         public const int DefaultBufferSize = 512;
     }
 }
