9696cf8821f8bc7c1ac114694afd60d364c68bfa
c6burns <chris@fursman.com>
2019-04-04 05:35:14 -0600

    added initial framework concept

---
+++ Assets/Mirror/Runtime/Buffers/BufferManager.cs
@@ -0,0 +1,174 @@
+#define MIRROR_BUFFER_STACK_DEBUG
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Mirror.Buffers
+{
+    public static class BufferManager
+    {
+        #region AquiredRef and AquiredRef internal classes
+        internal class AcquiredRef<T>
+        {
+            private readonly T _classRef;
+#if MIRROR_BUFFER_STACK_DEBUG
+            private readonly string _allocStack;
+            private readonly DateTime _allocTime;
+#endif
+
+            public AcquiredRef(T classInstance)
+            {
+                _classRef = classInstance;
+#if MIRROR_BUFFER_STACK_DEBUG
+                _allocStack = Environment.StackTrace;
+                _allocTime = DateTime.Now;
+#endif
+            }
+
+            public T Reference()
+            {
+                return _classRef;
+            }
+
+#if MIRROR_BUFFER_STACK_DEBUG
+            public string StackTrace()
+            {
+                return _allocStack;
+            }
+
+            public DateTime AllocTime()
+            {
+                return _allocTime;
+            }
+#endif
+        }
+
+        internal class AcquiredRefList<T>
+        {
+            private Dictionary<T, AcquiredRef<T>> _refMap = new Dictionary<T, AcquiredRef<T>>();
+            private Stack<AcquiredRef<T>> _refFreeStack = new Stack<AcquiredRef<T>>();
+            public T Acquire()
+            {
+                if (_refFreeStack.Count > 0)
+                {
+                    return _refFreeStack.Pop().Reference();
+
+                }
+
+                return default;
+            }
+
+            public void Add(T classRef)
+            {
+                _refMap.Add(classRef, new AcquiredRef<T>(classRef));
+            }
+
+            public void Release(T classRef)
+            {
+                if (_refMap.TryGetValue(classRef, out AcquiredRef<T> val))
+                {
+                    _refFreeStack.Push(val);
+                }
+            }
+
+            public bool HasLeakedReferences()
+            {
+                return (_refFreeStack.Count == _refMap.Count);
+            }
+
+            public void DebugReferences()
+            {
+#if MIRROR_BUFFER_STACK_DEBUG
+                while (_refFreeStack.Count > 0)
+                {
+                    AcquiredRef<T> classRef = _refFreeStack.Pop();
+                    if (!_refMap.Remove(classRef.Reference()))
+                    {
+                        Debug.LogErrorFormat("Not in map: {0}\n{1}", classRef.AllocTime().ToShortTimeString(), classRef.StackTrace());
+                    }
+
+                    foreach (KeyValuePair<T, AcquiredRef<T>> entry in _refMap)
+                    {
+                        Debug.LogWarningFormat("Not released: {0}\n{1}", entry.Value.AllocTime().ToShortTimeString(), entry.Value.StackTrace());
+                    }
+                }
+#else
+                Debug.LogWarning("Reference details not collected without MIRROR_ALLOC_STACK_DEBUG defined");
+#endif
+            }
+        }
+        #endregion
+
+
+        private static BufferAllocator _defaultBufferAllocator = new BufferAllocator();
+        private static IBufferAllocator _bufferAllocator = _defaultBufferAllocator;
+        private static AcquiredRefList<NetworkWriter> _writerList = new AcquiredRefList<NetworkWriter>();
+        private static AcquiredRefList<NetworkReader> _readerList = new AcquiredRefList<NetworkReader>();
+
+        static BufferManager()
+        {
+            // cctor aka static ctor
+        }
+
+        public static void RegisterAllocator(IBufferAllocator allocator)
+        {
+            _bufferAllocator = allocator;
+        }
+
+        public static void UnregisterAllocator(IBufferAllocator allocator)
+        {
+            if (_bufferAllocator == allocator)
+            {
+                _bufferAllocator = _defaultBufferAllocator;
+            }
+        }
+
+        public static IBuffer AcquireBuffer(int minSizeInBytes)
+        {
+            return _bufferAllocator.Acquire(minSizeInBytes);
+        }
+
+        public static IBuffer ReacquireBuffer(IBuffer rentedBuffer, int newMinSizeInBytes)
+        {
+            return _bufferAllocator.Reacquire(rentedBuffer, newMinSizeInBytes);
+        }
+
+        public static void ReleaseBuffer(IBuffer rentedBuffer)
+        {
+            _bufferAllocator.Release(rentedBuffer);
+        }
+
+        public static NetworkWriter AcquireWriter()
+        {
+            NetworkWriter rv = _writerList.Acquire();
+            if (rv == null)
+            {
+                rv = new NetworkWriter();
+                _writerList.Add(rv);
+            }
+            return rv;
+        }
+
+        public static void ReleaseWriter(NetworkWriter writer)
+        {
+            _writerList.Release(writer);
+        }
+
+        public static NetworkReader AcquireReader(byte[] buffer)
+        {
+            NetworkReader rv = _readerList.Acquire();
+            if (rv == null)
+            {
+                rv = new NetworkReader(buffer);
+                _readerList.Add(rv);
+            }
+            return rv;
+        }
+
+        public static void ReleaseReader(NetworkReader reader)
+        {
+            _readerList.Release(reader);
+        }
+    }
+}
---
+++ Assets/Mirror/Runtime/Buffers/BufferUtil.cs
@@ -0,0 +1,401 @@
+using System.Text;
+using System.Runtime.CompilerServices;
+
+//
+// TODO: test actual effectiveness and assembly size change of aggressive inlining (JIT likely is already doing it)
+//
+
+namespace Mirror.Buffers
+{
+    public static class BufferUtil
+    {
+        #region NextPow2: rounding up to closest power of two
+        public static sbyte NextPow2(sbyte val) => (sbyte)NextPow2((byte)val);
+        public static byte NextPow2(byte val)
+        {
+            val = (byte)(val < 2 ? 2 : val);
+            val--;
+            val |= (byte)(val >> 1);
+            val |= (byte)(val >> 2);
+            val |= (byte)(val >> 4);
+            val++;
+            return val;
+        }
+
+        public static short NextPow2(short val) => (short)NextPow2((ushort)val);
+        public static ushort NextPow2(ushort val)
+        {
+            val = (ushort)(val < 2 ? 2 : val);
+            val--;
+            val |= (ushort)(val >> 1);
+            val |= (ushort)(val >> 2);
+            val |= (ushort)(val >> 4);
+            val |= (ushort)(val >> 8);
+            val++;
+            return val;
+        }
+
+        public static int NextPow2(int val) => (int)NextPow2((uint)val);
+        public static uint NextPow2(uint val)
+        {
+            val = (val < 2 ? 2 : val);
+            val--;
+            val |= val >> 1;
+            val |= val >> 2;
+            val |= val >> 4;
+            val |= val >> 8;
+            val |= val >> 16;
+            val++;
+            return val;
+        }
+
+        public static long NextPow2(long val) => (long)NextPow2((ulong)val);
+        public static ulong NextPow2(ulong val)
+        {
+            val = (val < 2 ? 2 : val);
+            val--;
+            val |= val >> 1;
+            val |= val >> 2;
+            val |= val >> 4;
+            val |= val >> 8;
+            val |= val >> 16;
+            val |= val >> 32;
+            val++;
+            return val;
+        }
+        #endregion
+
+        #region SwapBytes: endian swapping
+        public static short SwapBytes(short input) => (short)SwapBytes((ushort)input);
+        public static ushort SwapBytes(ushort input)
+        {
+            return (ushort)(((input & 0x00FFU) << 8) |
+                            ((input & 0xFF00U) >> 8));
+        }
+
+        public static int SwapBytes(int input) => (int)SwapBytes((uint)input);
+        public static uint SwapBytes(uint input)
+        {
+            return ((input & 0x000000FFU) << 24) |
+                   ((input & 0x0000FF00U) << 8) |
+                   ((input & 0x00FF0000U) >> 8) |
+                   ((input & 0xFF000000U) >> 24);
+        }
+
+        public static long SwapBytes(long input) => (long)SwapBytes((ulong)input);
+        public static ulong SwapBytes(ulong input)
+        {
+            return (((input & 0x00000000000000FFUL) << 56) |
+                    ((input & 0x000000000000FF00UL) << 40) |
+                    ((input & 0x0000000000FF0000UL) << 24) |
+                    ((input & 0x00000000FF000000UL) << 8) |
+                    ((input & 0x000000FF00000000UL) >> 8) |
+                    ((input & 0x0000FF0000000000UL) >> 24) |
+                    ((input & 0x00FF000000000000UL) >> 40) |
+                    ((input & 0xFF00000000000000UL) >> 56));
+        }
+        #endregion
+
+        #region UnsafeCopy#: raw pointer based binary copy for exact sizes from 1 - 8 bytes
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeCopy1(byte* pdst, byte* psrc)
+        {
+            *pdst = *psrc;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeCopy2(byte* pdst, byte* psrc)
+        {
+            *(ushort*)pdst = *(ushort*)psrc;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeCopy3(byte* pdst, byte* psrc)
+        {
+            *(ushort*)pdst = *(ushort*)psrc;
+            *(pdst + BufferConstants.SizeOfShort) = *(psrc + BufferConstants.SizeOfShort);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeCopy4(byte* pdst, byte* psrc)
+        {
+            *(uint*)pdst = *(uint*)psrc;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeCopy5(byte* pdst, byte* psrc)
+        {
+            *(uint*)pdst = *(uint*)psrc;
+            *(pdst + BufferConstants.SizeOfInt) = *(psrc + BufferConstants.SizeOfInt);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeCopy6(byte* pdst, byte* psrc)
+        {
+            *(uint*)pdst = *(uint*)psrc;
+            *(ushort*)(pdst + BufferConstants.SizeOfInt) = *(ushort*)(psrc + BufferConstants.SizeOfInt);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeCopy7(byte* pdst, byte* psrc)
+        {
+            *(uint*)pdst = *(uint*)psrc;
+            *(ushort*)(pdst + BufferConstants.SizeOfInt) = *(ushort*)(psrc + BufferConstants.SizeOfInt);
+            *(pdst + BufferConstants.SizeOfInt + BufferConstants.SizeOfShort) = *(psrc + BufferConstants.SizeOfInt + BufferConstants.SizeOfShort);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeCopy8(byte* pdst, byte* psrc)
+        {
+            *(ulong*)pdst = *(ulong*)psrc;
+        }
+        #endregion
+
+        #region UnsafeWrite: unsafe binary writing using fixed pinning
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void UnsafeWrite(byte[] dst, int dstOffset, bool boolSrc) => UnsafeWrite(dst, dstOffset, (byte)(boolSrc ? 1 : 0));
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void UnsafeWrite(byte[] dst, int dstOffset, sbyte sbyteSrc) => UnsafeWrite(dst, dstOffset, (byte)sbyteSrc);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeWrite(byte[] dst, int dstOffset, byte byteSrc)
+        {
+            fixed (byte* pdst = &dst[dstOffset])
+            {
+                *pdst = byteSrc;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void UnsafeWrite(byte[] dst, int dstOffset, short shortSrc) => UnsafeWrite(dst, dstOffset, (ushort)shortSrc);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeWrite(byte[] dst, int dstOffset, ushort ushortSrc)
+        {
+            fixed (byte* pdst = &dst[dstOffset])
+            {
+                *(ushort*)pdst = ushortSrc;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void UnsafeWrite(byte[] dst, int dstOffset, int intSrc) => UnsafeWrite(dst, dstOffset, (uint)intSrc);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeWrite(byte[] dst, int dstOffset, uint uintSrc)
+        {
+            fixed (byte* pdst = &dst[dstOffset])
+            {
+                *(uint*)pdst = uintSrc;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void UnsafeWrite(byte[] dst, int dstOffset, long longSrc) => UnsafeWrite(dst, dstOffset, (ulong)longSrc);
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeWrite(byte[] dst, int dstOffset, ulong ulongSrc)
+        {
+            fixed (byte* pdst = &dst[dstOffset])
+            {
+                *(ulong*)pdst = ulongSrc;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeWrite(byte[] dst, int dstOffset, float floatSrc)
+        {
+            fixed (byte* pdst = &dst[dstOffset])
+            {
+                *(float*)pdst = floatSrc;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeWrite(byte[] dst, int dstOffset, double doubleSrc)
+        {
+            fixed (byte* pdst = &dst[dstOffset])
+            {
+                *(double*)pdst = doubleSrc;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeWrite(byte[] dst, int dstOffset, string stringSrc)
+        {
+            fixed (char* s = stringSrc)
+            fixed (byte* pdst = &dst[dstOffset])
+            {
+                Encoding.UTF8.GetBytes(s, stringSrc.Length, pdst, stringSrc.Length);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeWrite(byte[] dst, int dstOffset, byte[] src, int srcOffset, int byteLength)
+        {
+            int longWriteLimit = (int)(byteLength & 0xfffffff8);
+            fixed (byte* psrc = &src[srcOffset])
+            fixed (byte* pdst = &dst[dstOffset])
+            {
+                // write anything over 8 bytes as a series of long*
+                for (int cursor = 0; cursor < longWriteLimit; cursor += BufferConstants.SizeOfLong)
+                {
+                    UnsafeCopy8(pdst + cursor, psrc + cursor);
+                }
+
+                // write anything remaining under 8 bytes
+                switch (byteLength & 0x00000007)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        UnsafeCopy1(pdst + longWriteLimit, psrc + longWriteLimit);
+                        break;
+                    case 2:
+                        UnsafeCopy2(pdst + longWriteLimit, psrc + longWriteLimit);
+                        break;
+                    case 3:
+                        UnsafeCopy3(pdst + longWriteLimit, psrc + longWriteLimit);
+                        break;
+                    case 4:
+                        UnsafeCopy4(pdst + longWriteLimit, psrc + longWriteLimit);
+                        break;
+                    case 5:
+                        UnsafeCopy5(pdst + longWriteLimit, psrc + longWriteLimit);
+                        break;
+                    case 6:
+                        UnsafeCopy6(pdst + longWriteLimit, psrc + longWriteLimit);
+                        break;
+                    case 7:
+                        UnsafeCopy7(pdst + longWriteLimit, psrc + longWriteLimit);
+                        break;
+                }
+            }
+        }
+        #endregion
+
+        #region UnsafeRead: unsafe binary reading using fixed pinning
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref bool boolDst, byte[] src, int srcOffset)
+        {
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                boolDst = (*psrc == 0) ? false : true;
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref sbyte sbyteDst, byte[] src, int srcOffset)
+        {
+            fixed (void* pdst = &sbyteDst)
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                UnsafeCopy1((byte*)pdst, psrc);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref byte byteDst, byte[] src, int srcOffset)
+        {
+            fixed (byte* pdst = &byteDst)
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                UnsafeCopy1(pdst, psrc);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref short shortDst, byte[] src, int srcOffset)
+        {
+            fixed (void* pdst = &shortDst)
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                UnsafeCopy2((byte*)pdst, psrc);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref ushort ushortDst, byte[] src, int srcOffset)
+        {
+            sbyte turnt = 1;
+            byte[] snaco = new byte[] { 3, 4 };
+            UnsafeRead(ref turnt, snaco, 0);
+
+            fixed (void* pdst = &ushortDst)
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                UnsafeCopy2((byte*)pdst, psrc);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref int intSrc, byte[] src, int srcOffset)
+        {
+            fixed (void* pdst = &intSrc)
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                UnsafeCopy4((byte*)pdst, psrc);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref uint uintDst, byte[] src, int srcOffset)
+        {
+            fixed (void* pdst = &uintDst)
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                UnsafeCopy4((byte*)pdst, psrc);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref long longDst, byte[] src, int srcOffset)
+        {
+            fixed (void* pdst = &longDst)
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                UnsafeCopy8((byte*)pdst, psrc);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref ulong ulongDst, byte[] src, int srcOffset)
+        {
+            fixed (void* pdst = &ulongDst)
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                UnsafeCopy8((byte*)pdst, psrc);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref float floatDst, byte[] src, int srcOffset)
+        {
+            fixed (void* pdst = &floatDst)
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                UnsafeCopy4((byte*)pdst, psrc);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref double doubleDst, byte[] src, int srcOffset)
+        {
+            fixed (void* pdst = &doubleDst)
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                UnsafeCopy8((byte*)pdst, psrc);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(ref string stringDst, byte[] src, int srcOffset, int byteLength)
+        {
+            fixed (byte* psrc = &src[srcOffset])
+            {
+                stringDst = Encoding.UTF8.GetString(psrc, byteLength);
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static unsafe void UnsafeRead(byte[] dst, int dstOffset, byte[] src, int srcOffset, int byteLength) => UnsafeWrite(src, srcOffset, dst, dstOffset, byteLength);
+        #endregion
+    }
+}
